647311fe893f0a8a17f507fdda323860c3212bd8
Tiago Barboza <ticolima16@gmail.com>
2024-10-11 11:52:29 -0300

    Added CommandLineRunner

--- src/main/java/br/tiagobarboza/screenmatchV1/ScreenmatchV1Application.java
+++ src/main/java/br/tiagobarboza/screenmatchV1/ScreenmatchV1Application.java
@@ -1,13 +1,18 @@
 package br.tiagobarboza.screenmatchV1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchV1Application {
+public class ScreenmatchV1Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchV1Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Ola, MUNDO!");
+	}
 }
