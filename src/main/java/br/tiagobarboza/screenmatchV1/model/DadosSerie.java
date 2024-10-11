772c62c732a1ec757cf0b119bf3232fe52dea8c0
Tiago Barboza <ticolima16@gmail.com>
2024-10-11 13:18:59 -0300

    update class DadosSerie. Added @JsonIgnoreProperties

--- src/main/java/br/tiagobarboza/screenmatchV1/model/DadosSerie.java
+++ src/main/java/br/tiagobarboza/screenmatchV1/model/DadosSerie.java
@@ -1,10 +1,11 @@
 package br.tiagobarboza.screenmatchV1.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(
                          @JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
-                         @JsonAlias("imdbRating") String avaliacao,
-                         @JsonAlias("imdbVotes") String votos ) {
+                         @JsonAlias("imdbRating") String avaliacao) {
 }
