3d7780939c89d9db4bf5dffbbedb305d34f39b84
Tiago Barboza <ticolima16@gmail.com>
2024-10-11 11:49:37 -0300

    Project started

---
+++ src/main/java/br/tiagobarboza/screenmatchV1/ScreenmatchV1Application.java
@@ -0,0 +1,13 @@
+package br.tiagobarboza.screenmatchV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/tiagobarboza/screenmatchV1/ScreenmatchV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.tiagobarboza.screenmatchV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
